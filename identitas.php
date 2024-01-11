f74c2a7075936996606e64baf7be4900bf87cac4
maulana2405 <156313855+maulana2405@users.noreply.github.com>
2024-01-11 18:22:05 +0700

    Add files via upload

---
+++ identitas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Php identitas</title>
+</head>
+<body>
+
+<h1>Berikut adl identitas saya</h1>
+
+<ul>
+    <li><strong>Nama:</strong> maulana anggas</li>
+    <li><strong>Tempat Lahir:</strong> yogyakarta</li>
+    <li><strong>Tanggal Lahir:</strong> 24 Mei 2004</li>
+    <li><strong>Alamat:</strong> Jl suryodiningratan no 63</li>
+    <li><strong>Pekerjaan:</strong> Mahasiswa </li>
+</ul>
+
+</body>
+</html>
